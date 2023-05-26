54f1c0e64fa754972f9b83bbff28866a0a5982dc
Ibrocode94 <ibrahimozigis0@gmail.com>
2023-05-26 22:22:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import Tasks  from "./components/Tasks";
+import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddtask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -26,28 +27,33 @@ function App() {
   ]);
 
   const addTask = (task) => {
-    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...task, newTask]);
+  };
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-  }
-    //Delete task
-    const deleteTask = (id) => {
-      setTasks(tasks.filter((task) => task.id !== id))
-    }
-
-    //Toggle Reminder
-    const  toggleReminder = (id) => {
-      setTasks(tasks.map((task) => task.id == id ? {...task, reminder: !task.reminder} : task))
-
-    }
-
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id == id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header />
-      <AddTask  onAdd = {addTask}/>
-     {tasks.length> 0 ? (<Tasks tasks={tasks} onDelete=
-       {deleteTask} onToggle = {toggleReminder}/>
-        ):( "No Task to Show")}
+      <Header onAdd={() => setShowAddTask(!showAddtask)} showAdd= {showAddtask} />
+      {showAddtask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task to Show"
+      )}
     </div>
   );
 }
